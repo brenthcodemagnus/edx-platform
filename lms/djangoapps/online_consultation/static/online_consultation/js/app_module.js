33a881769a05b74c70864fca1a81abc274a4ea44
Brenth Andrew J. Miras <brenth.codemagnus@gmail.com>
2015-08-11 12:07:43 +0000

    Added basic routing on consultations view

--- lms/djangoapps/online_consultation/static/online_consultation/js/app_module.js
+++ lms/djangoapps/online_consultation/static/online_consultation/js/app_module.js
@@ -1,12 +1,31 @@
 'use strict';
-define(['angular'],function(angular){
+define(['angular','ui-router'],function(angular, ui_router){
 
 	angular
-		.module("app", [])
+		.module("app", ['ui.router'])
+		
 		.controller("MainController", ["$scope", function($scope){
 			$scope.title = "This is the title";
 
 			$scope.content = "This is the content";
 		}])
+		
+		.config(function($stateProvider, $urlRouterProvider) {
+		    //
+		    // For any unmatched url, redirect to /state1
+		    $urlRouterProvider.otherwise("/home");
+		    //
+		    // Now set up the states
+		    $stateProvider
+		        .state('home', {
+		            url: "/home",
+		            templateUrl: staticUrl("templates/home.html"),
+		            controller: "MainController"
+		        })
+		        .state('about', {
+		            url: "/about",
+		            templateUrl: staticUrl("templates/about.html")
+		        })
+		});
 		
 });
--- lms/djangoapps/online_consultation/static/online_consultation/js/main.js
+++ lms/djangoapps/online_consultation/static/online_consultation/js/main.js
@@ -1,10 +1,14 @@
 require.config({
 	paths: {
-		'angular': '../bower_components/angularjs/angular'
+		'angular': '../bower_components/angularjs/angular',
+		'ui-router': '../bower_components/angular-ui-router/release/angular-ui-router'
 	},
 	shim: {
 		'angular': {
 			'exports': 'angular'
+		},
+		'ui-router': {
+			'exports': 'ui-router'
 		}
 	}
 });
