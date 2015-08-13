eddd448c8f2929e0623f35a70539b73db05485df
Brenth Andrew J. Miras <brenth.codemagnus@gmail.com>
2015-08-13 07:18:27 +0000

    Add model ConsultationSchedule

--- lms/djangoapps/online_consultation/models.py
+++ lms/djangoapps/online_consultation/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
+class ConsultationSchedule(models.Model):
+	"""docstring for ConsultationSchedule"""
+	student = models.ForeignKey(User, related_name="student")
+	instructor = models.ForeignKey(User, blank=False, null=False, related_name="instructor")
+	start_date = models.DateTimeField(default=datetime.now)
+	end_date = models.DateTimeField(default=datetime.now)
+	session_id = models.CharField(max_length=128)
+
+	def is_available(self):
+		if self.student:
+			return False
+		else:
+			return True
+
