4aeac09ebd33f80379bc2f58a2818d8861566e59
Brenth Andrew J. Miras <brenth.codemagnus@gmail.com>
2015-08-13 00:34:05 +0000

    Fetch instructors from server to instructors view

--- lms/djangoapps/online_consultation/static/online_consultation/js/app_module.js
+++ lms/djangoapps/online_consultation/static/online_consultation/js/app_module.js
@@ -10,24 +10,59 @@ define(['angular','ui-router'],function(angular, ui_router){
 			$scope.content = "This is the content";
 		}])
 
-		.controller("InstructorsController", ["$scope", function($scope){
-			$scope.instructors = [
-				{
-					id: 1,
-					username: "brenthmiras"
-				},
-				{
-					id: 2,
-					username: "nferocious"
-				},
-				{
-					id: 3,
-					username: "rubberdont"
-				}
-			];
+		.service("InstructorsService", ["$q", "$http", function($q, $http){
+			var urls = {
+				instructors: "http://" + config.BASE_URL + "/api/consultation/v0/instructors/" + config.COURSE_ID
+			};
+
+			this.getInstructors = function(){
+				return $http.get(urls.instructors);
+			};
+
+		}])
+
+		.controller("InstructorsController", ["$scope", "InstructorsService", function($scope, InstructorsService){
+			// $scope.instructors = [
+			// 	{
+			// 		id: 1,
+			// 		username: "brenthmiras"
+			// 	},
+			// 	{
+			// 		id: 2,
+			// 		username: "nferocious"
+			// 	},
+			// 	{
+			// 		id: 3,
+			// 		username: "rubberdont"
+			// 	}
+			// ];
+
+			//$scope.instructors = [];
 
 			$scope.placeholder_url = staticUrl("images/male-profile-placeholder.jpg");
 
+			$scope.getInstructors = function(){
+				InstructorsService
+					.getInstructors()
+					.then(
+						function(response){
+							console.log(response);
+							$scope.instructors = response.data;
+						},
+						function(error){
+							alert("Cannot fetch instructors");
+							console.log(error)
+						}
+					);
+			};
+
+			$scope.noInstructors = function(){
+				return $scope.instructors && $scope.instructors.length == 0;
+			};
+
+			(function init(){
+				$scope.getInstructors();
+			})();
 
 		}])
 		
