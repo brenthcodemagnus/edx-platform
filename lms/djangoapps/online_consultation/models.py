4fc4c0a67272eac314314663b38968ac0496a5e3
Brenth Andrew J. Miras <brenth.codemagnus@gmail.com>
2015-08-14 04:09:51 +0000

    Added field ConsultationSchedule.course

--- lms/djangoapps/online_consultation/models.py
+++ lms/djangoapps/online_consultation/models.py
@@ -7,6 +7,7 @@ class ConsultationSchedule(models.Model):
 	"""docstring for ConsultationSchedule"""
 	student = models.ForeignKey(User, related_name="student")
 	instructor = models.ForeignKey(User, blank=False, null=False, related_name="instructor")
+	course = models.CharField(max_length=256)
 	start_date = models.DateTimeField(default=datetime.now)
 	end_date = models.DateTimeField(default=datetime.now)
 	session_id = models.CharField(max_length=128)
