fb597f48b198d3b329c9b84f4d506d4dada2e0cf
Brenth Andrew J. Miras <brenth.codemagnus@gmail.com>
2015-08-17 11:36:29 +0000

    Added validator for ConsultationSchedule.course

--- lms/djangoapps/online_consultation/models.py
+++ lms/djangoapps/online_consultation/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
+from validators import validate_course_id
 
 # Create your models here.
 class ConsultationSchedule(models.Model):
 	"""docstring for ConsultationSchedule"""
 	student = models.ForeignKey(User, related_name="student")
 	instructor = models.ForeignKey(User, blank=False, null=False, related_name="instructor")
-	course = models.CharField(max_length=256)
+	course = models.CharField(max_length=256, blank=False, null=False, validators=[validate_course_id])
 	start_date = models.DateTimeField(default=datetime.now)
 	end_date = models.DateTimeField(default=datetime.now)
 	session_id = models.CharField(max_length=128)
---
+++ lms/djangoapps/online_consultation/validators.py
@@ -0,0 +1,19 @@
+from django.core.exceptions import ValidationError
+from opaque_keys import InvalidKeyError
+from opaque_keys.edx.keys import CourseKey
+from xmodule.modulestore.django import modulestore
+
+def validate_course_id(course_id):
+    # Ensure valid course_id
+    if course_id_string is None:
+        raise ValidationError('%s should not be None' % course_id)
+
+    try:
+        course_id = CourseKey.from_string(course_id)
+    except InvalidKeyError:
+        raise ValidationError('%s is an invalid course key' % course_id)
+
+    # Ensure the course exists
+    course_module = modulestore().get_course(course_id)
+    if course_module is None:  # course is None if not found
+        raise ValidationError('there is no such course with id: %s' % course_id)
