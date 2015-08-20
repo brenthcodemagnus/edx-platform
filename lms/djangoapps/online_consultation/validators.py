b74d24f26a7be386f01a0979f1f9c309e99fc4b1
Brenth Andrew J. Miras <brenth.codemagnus@gmail.com>
2015-08-20 04:31:02 +0000

    Changed validators.validate_course_id to validate key only and not existence

--- lms/djangoapps/online_consultation/validators.py
+++ lms/djangoapps/online_consultation/validators.py
@@ -3,6 +3,8 @@ from opaque_keys import InvalidKeyError
 from opaque_keys.edx.keys import CourseKey
 from xmodule.modulestore.django import modulestore
 
+# Checks only for key validity
+# not for existence of course in database
 def validate_course_id(course_id):
     # Ensure valid course_id
     if course_id_string is None:
@@ -14,6 +16,6 @@ def validate_course_id(course_id):
         raise ValidationError('%s is an invalid course key' % course_id)
 
     # Ensure the course exists
-    course_module = modulestore().get_course(course_id)
-    if course_module is None:  # course is None if not found
-        raise ValidationError('there is no such course with id: %s' % course_id)
+    # course_module = modulestore().get_course(course_id)
+    # if course_module is None:  # course is None if not found
+    #     raise ValidationError('there is no such course with id: %s' % course_id)
