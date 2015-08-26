b2ee059d03ff3c9825c40f5f43bde7492d252517
Brenth Andrew J. Miras <brenth.codemagnus@gmail.com>
2015-08-26 03:59:45 +0000

    Modified instructor schedules controller to fetch instructor shcedules from server

--- lms/djangoapps/online_consultation/static/online_consultation/js/instructors-schedules-module.js
+++ lms/djangoapps/online_consultation/static/online_consultation/js/instructors-schedules-module.js
@@ -3,39 +3,39 @@ define(['angular', 'ui-bootstrap'],function( angular, ui_bootstrap){
 	
 	angular.module("instructors-schedules-module", ['ui.bootstrap'])
 
-	.controller("InstructorSchedulesController", ["$scope", "$stateParams", "$state", function($scope, $stateParams, $state){
+	.service("InstructorSchedulesService", ["$http", "$q", function($http, $q){
+		// this function gets schedules created by
+		// instructor (identified by username)
+		// for this course
+
+		var urls = {
+			INSTRUCTOR_SCHEDULES: "http://" + config.BASE_URL + "/api/consultation/v0/"+config.COURSE_ID+"/schedules"
+		};
+
+		this.getInstructorSchedules = function(username){
+			return $http.get(urls.INSTRUCTOR_SCHEDULES + "/" + username);
+		};
+	}])
+
+	.controller("InstructorSchedulesController", ["$scope", "$stateParams", "$state", "InstructorSchedulesService", function($scope, $stateParams, $state, InstructorSchedulesService){
 		
 		var instructorUsername = $stateParams.username;
 		
 		console.log("username is: " + instructorUsername);
 
-		$scope.getSchedules = function(){
-			// InstructorsService
-			// 	.getInstructors()
-			// 	.then(
-			// 		function(response){
-			// 			console.log(response);
-			// 			$scope.instructors = response.data;
-			// 		},
-			// 		function(error){
-			// 			alert("Cannot fetch instructors");
-			// 			console.log(error)
-			// 		}
-			// 	);
-			$scope.schedules = [
-				{
-					id: 1,
-					text: "sched1"
-				},
-				{
-					id: 2,
-					text: "sched2"
-				},
-				{
-					id: 3,
-					text: "sched3"
-				}
-			];
+		$scope.getSchedules = function(username){
+			InstructorSchedulesService
+				.getInstructorSchedules(username)
+				.then(
+					function(response){
+						console.log(response);
+						$scope.schedules = response.data;
+					},
+					function(error){
+						alert("Cannot fetch schedules");
+						console.log(error)
+					}
+				);
 		};
 
 		$scope.back = function(){
@@ -52,7 +52,7 @@ define(['angular', 'ui-bootstrap'],function( angular, ui_bootstrap){
 			if($scope.usernameExists(instructorUsername)){
 				console.log("filterByUsername will be called");
 				$scope.filterByUsername(instructorUsername);
-				$scope.getSchedules();
+				$scope.getSchedules(instructorUsername);
 			}
 			else{
 				$scope.back();
